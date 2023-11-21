002469bf91c2cda24e5f74a3c61c011a5e1b2503
Gabriel Teixeira <gabrielft8gft@gmail.com>
2023-11-21 16:18:26 -0300

    feat: adicionado a aceitação de outras rotas além de cardapio

--- Implementacao/Backend/app.js
+++ Implementacao/Backend/app.js
@@ -1,18 +1,23 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import clienteRouter from './routes/cliente.routes.js'
 import cardapioRouter from './routes/cardapio.routes.js'
+import pedidosRouter from './routes/pedidos.routes.js'
+import itensPedidosRouter from './routes/itensPedidos.routes.js'
+import enderecosRouter from './routes/enderecos.routes.js'
 
 const app = express()
 app.use(express.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extend: true }))
 
-app.use("/cliente", clienteRouter)
+// app.use("/cliente", clienteRouter)
 app.use("/cardapio", cardapioRouter)
+app.use("/pedidos", pedidosRouter)
+app.use("/itensPedidos", itensPedidosRouter)
+app.use("/enderecos", enderecosRouter)
 
 app.listen(3000, mensagemServisor)
 
 function mensagemServisor() {
-    console.log("Servidor rodando")
+    console.log("Servidor rodando com sucesso")
 }
